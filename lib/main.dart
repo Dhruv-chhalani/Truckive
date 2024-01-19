ffb9de9be51709209af78fbab2f24dd16b99c51d
Dhruv-chhalani <126203812+Dhruv-chhalani@users.noreply.github.com>
2024-01-19 22:30:26 +0530

    test2

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:loginuicolors/splash.dart';
-// ndbvhjsdvh
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
